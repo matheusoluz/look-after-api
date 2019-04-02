87a11d16b46a0fac22567e4de99727a0a4b79f3b
MatheusLuz <matheus.luz12@gmail.com>
2019-04-02 02:13:50 -0300

    Expõe arquivos estáticos

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,7 @@
 'use strict';
+var history = require('connect-history-api-fallback');
+var path = require('path');
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+module.exports = function(app) {
+  app.use(history({}));
 };
