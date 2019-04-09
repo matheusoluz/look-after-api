92d07db70ad82f780b6d04b6bca1454a1b56bbb4
MatheusLuz <matheus.luz12@gmail.com>
2019-04-09 01:52:58 -0300

    Adição de lógica para salvar o tempo para zerar o estoque

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,22 @@
 'use strict';
+const moment = require('moment');
 
 module.exports = (Product) => {
+  Product.observe('before save', hookUpdateCreatationDate(Product));
+};
 
+/***
+ *
+ * @param {object} ctx
+ */
+const hookUpdateCreatationDate = Product => async (ctx) => {
+  if (!ctx.isNewInstance) {
+    if (ctx.data && (ctx.data.purchasedQty || ctx.data.availableQty)) {
+      var a = moment(Date.now());
+      var b = moment(ctx.currentInstance.creatationDate);
+      ctx.data.timeToZeroed = ctx.data.availableQty * a.diff(b, 'minutes', true)
+      ctx.data.creatationDate = Date.now();
+    }
+  }
+  return true;
 };
