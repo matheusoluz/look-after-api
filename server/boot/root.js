a847a2083641d85fded30208c7be0221b9ea7498
MatheusLuz <matheus.luz12@gmail.com>
2019-04-08 22:41:08 -0300

    alteração de roteamento de acordo com variável de ambiente

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,7 +1,13 @@
 'use strict';
-var history = require('connect-history-api-fallback');
-var path = require('path');
+const history = require('connect-history-api-fallback');
 
-module.exports = function(app) {
-  app.use(history({}));
+module.exports = function(server) {
+  console.log('Process.env: ', process.env.NODE_ENV);
+  if (process.env.NODE_ENV === 'development') {
+    const router = server.loopback.Router();
+    router.get('/', server.loopback.status());
+    server.use(router);
+  } else {
+    server.use(history({}));
+  }
 };
